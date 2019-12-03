2c4efce40588bff93cb75453bcefd1b072763cf3
Mister M <muxaul.marin@gmail.com>
2019-12-03 23:20:52 +0500

    nothing

--- converter.bat
+++ converter.bat
@@ -1,1 +1,1 @@
-C:\Users\79824\AppData\Local\Programs\Python\Python36\Scripts\pyuic5.exe DialogDomainAdd.ui -o DialogDomainAdd.py
+C:\Users\79824\AppData\Local\Programs\Python\Python36\Scripts\pyuic5.exe DialogVariables.ui -o DialogVariables.py
